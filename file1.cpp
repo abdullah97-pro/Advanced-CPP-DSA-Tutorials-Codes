65c4931ca027f3c1f790a8aa32d6c2b193f71db7
abdullah97-pro <abdullahahmadi019@gmail.com>
2025-10-16 13:51:09 +0430

    exampel with common datatypes

--- file1.cpp
+++ file1.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 int main(){
 
-    string name;
-    cout<<"Enter your name: ";
-    cin>>name;
-    cout<<"Your name is "+name;
+    int age = 18;
+    string name = "Hamid";
+    float fee = 15.5;
+    bool isStudent = true;
+    cout<<name;
+    cout<<" is ";
+    cout<<age<<" years old "<<"and "<<name<<" is a student or not "<<isStudent<<". his fee is "<<fee;
 
     return 0;
 }
