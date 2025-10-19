e4ada00cbb9a41f0e2d471e0c40e2acfc0093aad
abdullah97-pro <abdullahahmadi019@gmail.com>
2025-10-19 13:49:49 +0430

    implecit and explecit

--- file1.cpp
+++ file1.cpp
@@ -11,15 +11,22 @@ int main(){
 //     cout << "Division: " << a / b << endl;
 //     //cout << "Remainder: " << a % b << endl;
 
-    int x = 12;
-    float f = 3.14;
-    double d = 3.1234567;
-    char a = 'A';
-    bool b = true;
-    int y = 0;
+    // int x = 12;
+    // float f = 3.14;
+    // double d = 3.1234567;
+    // char a = 'A';
+    // bool b = true;
+    // int y = 0;
 
-    // cout<<x<<d;
-    cout<<y;
+    // // cout<<x<<d;
+    // cout<<y;
+
+
+    int a = 5;
+    int b = 2;
+    // float f = a / b; //implecite
+    float y = (float)a / b;
+    cout<<a/b;
 
     return 0;
 }
