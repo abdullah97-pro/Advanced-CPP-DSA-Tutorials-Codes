9f0bf96641a12e118530538a37d4513dedb1ea9f
abdullah97-pro <abdullahahmadi019@gmail.com>
2025-10-19 13:41:58 +0430

    add signed and unsigned with variable values

--- file1.cpp
+++ file1.cpp
@@ -2,14 +2,24 @@
 using namespace std;
 int main(){
 
-int a, b;
-    cout << "Enter two numbers: ";
-    cin >> a >> b;
-    cout << "Sum: " << a + b << endl;
-    cout << "Difference: " << a - b << endl;
-    cout << "Product: " << a * b << endl;
-    cout << "Division: " << a / b << endl;
-    cout << "Remainder: " << a % b << endl;
+// int a, b;
+//     cout << "Enter two numbers: ";
+//     cin >> a >> b;
+//     cout << "Sum: " << a + b << endl;
+//     cout << "Difference: " << a - b << endl;
+//     cout << "Product: " << a * b << endl;
+//     cout << "Division: " << a / b << endl;
+//     //cout << "Remainder: " << a % b << endl;
+
+    int x = 12;
+    float f = 3.14;
+    double d = 3.1234567;
+    char a = 'A';
+    bool b = true;
+    int y = 0;
+
+    // cout<<x<<d;
+    cout<<y;
 
     return 0;
 }
