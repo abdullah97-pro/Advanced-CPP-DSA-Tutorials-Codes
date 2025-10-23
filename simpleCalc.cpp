6b6bacbd42634ba39ccbe0db3447cc75c80f7f27
abdullah97-pro <abdullahahmadi019@gmail.com>
2025-10-23 13:18:12 +0430

    take inputs from user in calculator

--- simpleCalc.cpp
+++ simpleCalc.cpp
@@ -3,6 +3,15 @@ using namespace std;
 int main() {
     double num1, num2;
     char ope;
-    
+
+    cout<<"Enter Number 1: ";
+    cin>>num1;
+
+    cout<<"Enter Operator: ";
+    cin>>ope;
+
+    cout<<"Enter Number 2: ";
+    cin>>num2;
+
     return 0;
 }
