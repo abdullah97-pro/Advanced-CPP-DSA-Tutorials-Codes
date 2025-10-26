2a8129c899a29d3f70119d0672fb6a6d35d0c6a4
abdullah97-pro <abdullahahmadi019@gmail.com>
2025-10-26 13:32:57 +0430

    Proof of & bitwise operator

--- bitwise.cpp
+++ bitwise.cpp
@@ -3,8 +3,20 @@ using namespace std;
 int main(){
 
     int x = 5;
-    int y = 1;
+    int y = 2;
     int result = x & y;
+
+    // 128 64 32 16 8 4 2 1
+    // 8   7  6  5  4 3 2 1
+
+    // A & B = AB 
+    // 1 & 1 = 1
+    // 1 & 0 = 0
+    // 0 & 1 = 0
+    // 0 & 0 = 0
+
+    // 5 -> 0101
+    // 2 -> 0010
 
     cout<<result<<endl;
 
