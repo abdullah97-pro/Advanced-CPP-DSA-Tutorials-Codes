e66eca3b9e9d480e0a1ed29db9d229614a9187c3
abdullah97-pro <abdullahahmadi019@gmail.com>
2025-11-02 13:32:35 +0430

    switch examples

--- file2.cpp
+++ file2.cpp
@@ -2,7 +2,33 @@
 using namespace std;
 int main(){
 
-    cout<<"Hello to CPP";
+    short day;
+
+    cout<<"Enter day: "<<endl;
+    cin>>day;
+
+    switch(day){
+        case 0:
+            cout<<"Saturday"<<endl;
+            break;
+        case 1:
+            cout<<"Sunday"<<endl;
+            break;
+        case 2:
+            cout<<"Monday"<<endl;
+            break;
+        case 3:
+            cout<<"Tuesday"<<endl;
+            break;
+        case 4:
+            cout<<"Wednessday"<<endl;
+            break;
+        case 5:
+            cout<<"Thursday"<<endl;
+            break;
+        default:
+            cout<<"Friday"<<endl;
+    }
 
     return 0;
 }
