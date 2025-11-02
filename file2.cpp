db1c9ba75f9e1750ca3dd779b1768f3f1f99f3e7
abdullah97-pro <abdullahahmadi019@gmail.com>
2025-11-02 13:39:07 +0430

    grade system examples

--- file2.cpp
+++ file2.cpp
@@ -2,33 +2,52 @@
 using namespace std;
 int main(){
 
-    short day;
+    // short day;
 
-    cout<<"Enter day: "<<endl;
-    cin>>day;
+    // cout<<"Enter day: "<<endl;
+    // cin>>day;
 
-    switch(day){
-        case 0:
-            cout<<"Saturday"<<endl;
-            break;
-        case 1:
-            cout<<"Sunday"<<endl;
-            break;
-        case 2:
-            cout<<"Monday"<<endl;
-            break;
-        case 3:
-            cout<<"Tuesday"<<endl;
-            break;
-        case 4:
-            cout<<"Wednessday"<<endl;
-            break;
-        case 5:
-            cout<<"Thursday"<<endl;
-            break;
-        default:
-            cout<<"Friday"<<endl;
+    // switch(day){
+    //     case 0:
+    //         cout<<"Saturday"<<endl;
+    //         break;
+    //     case 1:
+    //         cout<<"Sunday"<<endl;
+    //         break;
+    //     case 2:
+    //         cout<<"Monday"<<endl;
+    //         break;
+    //     case 3:
+    //         cout<<"Tuesday"<<endl;
+    //         break;
+    //     case 4:
+    //         cout<<"Wednessday"<<endl;
+    //         break;
+    //     case 5:
+    //         cout<<"Thursday"<<endl;
+    //         break;
+    //     default:
+    //         cout<<"Friday"<<endl;
+    // }
+
+
+    int grade;
+    cout<<"Enter Grade: ";
+    cin>>grade;
+
+    if(grade >= 90) {
+        cout<<"Grade: A"<<endl<<"Remark: Excellent"<<endl;
+    }else if(grade >= 75) {
+        cout<<"Grade: B"<<endl<<"Remark: Very Good"<<endl;
+    }else if(grade >= 60) {
+        cout<<"Grade: C"<<endl<<"Remark: Good"<<endl;
+    }else if(grade >= 50) {
+        cout<<"Grade: D"<<endl<<"Remark: Fair"<<endl;
+    }else {
+        cout<<"Grade: None"<<endl<<"Remark: Fail"<<endl;
     }
+
+
 
     return 0;
 }
